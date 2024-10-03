287de68b246da1956add43b4393e043a06a4d321
in2dm3-v <in2dm3-v@elorrieta.int>
2024-10-03 15:54:12 +0200

    Finalización de la parte compose
    Terminamos de darle unos retoques a la parque ocmpose de la calculadora

--- Calculadora_Compose/app/src/main/java/com/example/calculadora_compose/MainActivity.kt
+++ Calculadora_Compose/app/src/main/java/com/example/calculadora_compose/MainActivity.kt
@@ -4,29 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.calculadora_compose.ui.theme.Calculadora_ComposeTheme
@@ -36,6 +36,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Calculadora_ComposeTheme {
+                // Llama a la función para mostrar por  pantalla  la calculadora
                 CalculatorScreen()
             }
         }
@@ -44,30 +45,42 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CalculatorScreen() {
-    var displayText by remember { mutableStateOf("") } // Texto mostrado
+    // Texto mostrado
+    var displayText by rememberSaveable { mutableStateOf("") }
+    // Obtiene la orientacion de la pantalla
+    val configuration = LocalConfiguration.current
+    // Verifica si la orientación  es vertical
+    val isPortrait =
+        configuration.orientation == android.content.res.Configuration.ORIENTATION_PORTRAIT
 
+    // Colocamos  todos los elementos en una columna
     Column(
         modifier = Modifier
+            .background(Color.Black)
             .fillMaxSize()
             .padding(15.dp),
         verticalArrangement = Arrangement.SpaceBetween,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         // Pantalla de la calculadora
-       Text(
+
+        Text(
             text = displayText,
             style = MaterialTheme.typography.headlineLarge,
             modifier = Modifier
                 .fillMaxWidth()
-                .height(200.dp)
+                .height(if (isPortrait) 100.dp else 100.dp)
                 .padding(16.dp),
-            color = Color.Black,
+            color = Color.White,
             textAlign = androidx.compose.ui.text.style.TextAlign.End
         )
 
         // Filas de botones
         Column {
-            for (row in buttonRows) {
+            val buttonRowActual = if (isPortrait) buttonRows else buttonRowsA
+
+
+            for (row in buttonRowActual) {
                 Row(
                     modifier = Modifier.fillMaxWidth(),
                     horizontalArrangement = Arrangement.SpaceEvenly
@@ -82,9 +95,14 @@ fun CalculatorScreen() {
                     }
                 }
             }
+
+
         }
+
+
     }
 }
+
 
 // Lista de botones de la calculadora
 private val buttonRows = listOf(
@@ -94,29 +112,51 @@ private val buttonRows = listOf(
     listOf("0", "AC", "=", "+")
 )
 
+// Lista de botones de la calculadora
+private val buttonRowsA = listOf(
+    listOf("6", "7", "8", "9",  "/"),
+    listOf("2", "3", "4", "5",  "*"),
+    listOf("0", "1", "AC", "=", "+" ,"-"),
+
+)
+
 @Composable
 fun CalculatorButton(text: String, onClick: () -> Unit) {
-    Box(
+    Button(
+        onClick = onClick,
         modifier = Modifier
-            .size(70.dp)
-            .padding(4.dp)
-            .clickable(onClick = onClick)
-            .border(BorderStroke(3.dp, Color.Black), RoundedCornerShape(8.dp))
-            .padding(16.dp),
-        contentAlignment = Alignment.Center
+            .size(100.dp)
+            .padding(4.dp),
+        colors = ButtonDefaults.buttonColors(containerColor = Color.White),
+        border = BorderStroke(3.dp, Color.Black),
+        shape = RoundedCornerShape(20.dp)
     ) {
         Text(
             text = text,
             style = MaterialTheme.typography.titleMedium,
-            color = if (text == "C") Color.Red else Color.Blue
+            color = if (text == "AC") Color.Red else Color.Black
         )
     }
 }
 
-@Preview(showBackground = true)
+
+// Apaisado
+@Preview(
+    showBackground = true,
+    heightDp = 412,
+    widthDp = 873
+)
+// Vertical
+@Preview(
+    showBackground = true,
+    widthDp = 412,
+    heightDp = 873
+)
 @Composable
 fun CalculatorScreenPreview() {
     Calculadora_ComposeTheme {
         CalculatorScreen()
     }
 }
+
+
